bedd37668859964cfcd24f332bbef857f7cd0ee8
Ambas <ambas.chobsanti@paypay-corp.co.jp>
2024-01-17 09:19:16 +0900

    Initial Commit

---
+++ SampleRepo/SampleRepoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleRepoApp.swift
+//  SampleRepo
+//
+//  Created by Ambas Chobsanti on 2024/01/17.
+//
+
+import SwiftUI
+
+@main
+struct SampleRepoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
